d680ac7bbf3e14f168952e7de32c5342b0d861f5
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-17 16:23:36 -0500

    add intercepter

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 const app = express();
+// const data = [{username:"kitty", password:"unicorn"}]
 
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(function (req, res, next) {
+  console.log('in interceptor');
+  next()
+})
 
 app.get('/', function(req, res) {
-
-  res.render('home')
+console.log("first route");
+  res.render('login')
 
 })
+
+app.post('/login',function(req,res){
+// route corresponds with the action part on the form in the mustache file
+console.log("username is " +req.body.username);
+console.log("password is " +req.body.password);
+res.render('home')
+})
+// naming agents username and password come from the name attributes in the mustache html
 
 app.listen(3000, function() {
   console.log('Successfully started express application!');
